d9fe55f81e95bac3ada8bf42bf30361f15b07d80
tafy2392 <tafymunya@gmail.com>
2022-03-30 17:10:15 +0200

    add missing object

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -2,7 +2,7 @@ import asyncio
 
 import pytest  # type: ignore
 
-from database_operator.databases import Database
+from database_operator.databases import Database, PostgresConnection
 from database_operator.handlers import filter_on_postgres_update
 
 EXTENSIONS_BEFORE = ["postgis", "postrest"]
@@ -10,12 +10,20 @@ EXTENSIONS_AFTER = ["mathlib", "postgis"]
 
 DATABASE_FIELD_MAP = {"database": "DATABASE-NAME"}
 
+master_conn = PostgresConnection(
+    "test",
+    "test",
+    "test",
+    5432,
+    "test",
+)
+
 
 class TestExtractedFieldsFromSpec:
     @pytest.mark.asyncio
     async def test_from_spec(self):
         spec = DATABASE_FIELD_MAP
-        tps = await Database.from_spec(spec)
+        tps = await Database.from_spec(spec, master_conn)
         assert tps.database_name == "DATABASE-NAME"
         assert tps.drop_database is False
         assert tps.schemas == []
