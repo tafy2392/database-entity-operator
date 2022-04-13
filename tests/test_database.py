05d8233af718db4c9ab5b8f7422934d438c8ba15
tafy2392 <tafymunya@gmail.com>
2022-04-13 17:20:12 +0200

    Remove pytest fixture

--- tests/test_database.py
+++ tests/test_database.py
@@ -7,19 +7,6 @@ import pytest  # type: ignore
 
 
 @pytest.fixture(scope="session")
-def http_service(docker_ip, docker_services):
-    """Ensure that HTTP service is up and responsive."""
-
-    # `port_for` takes a container port and returns the corresponding host port
-    port = docker_services.port_for("httpbin", 80)
-    url = "http://{}:{}".format(docker_ip, port)
-    docker_services.wait_until_responsive(
-        timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
-    )
-    return url
-
-
-@pytest.fixture(scope="session")
 @asynccontextmanager
 async def db_connection(docker_services, docker_ip):
     """
@@ -50,7 +37,6 @@ async def test_conn(mocker, db_connection):
     yield True
 
 
-@pytest.mark.usefixtures("test_conn")
 class TestMock(unittest.TestCase):
     def test_program(self):
         #  this test is using mocked database connection.
